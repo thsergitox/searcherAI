1d3c7134de43106e5df1594cc684b3bd647b7e41
thsergitox <sergiopezoj@gmail.com>
2024-11-27 04:36:36 -0500

    refactor: remove unused CSS imports from pdf component

--- components/pdf.tsx
+++ components/pdf.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import "react-pdf/dist/esm/Page/TextLayer.css";
+
 
 // You might need to specify the workerSrc to avoid issues in certain setups
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
